5f630dafcf4709de319b56ffbfd0b465bcdcab13
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-07-02 13:17:54 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,7 +49,7 @@ class Song
  def self.new_from_filename(path)
    file = path.chomp(".mp3").split(" - ")
    new_song = Song.new(file[0])
-   Artist.find_or_create_by_name(file[2])
+   new_song.artist.name = [1]
    Genre.find_or_create_by_name(file[1])
  end
 
