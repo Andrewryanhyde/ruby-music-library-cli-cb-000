37eec76a92e530f27f35d851d9f98a167b22d744
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-07-02 12:50:27 +0000

    automatically backed up by learn

--- lib/musicimporter.rb
+++ lib/musicimporter.rb
@@ -3,8 +3,18 @@ class MusicImporter
   attr_accessor :path
 
   def inititalize(path)
-
-
+    @path = path
   end
 
+
+  def files
+     Dir.entries(path).select {|f| !File.directory? f }
+  end
+
+  def self.import 
+    filename = self.files
+    filename.each do |file| 
+      Song.create_from_filename(file)
+  end
+  end
 end
