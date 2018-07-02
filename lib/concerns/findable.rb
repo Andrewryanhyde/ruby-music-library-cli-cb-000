c2f24ee90429b5c572585b2f5513fdf4a706c760
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-07-02 12:09:10 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,7 +1,15 @@
 module Concerns::Findable
 
   def self.find_by_name(song)
-    self.all.find { |x| x.name == song}
+    self.all.find { |x| x.name == song} ||
+  end
+
+  def self.find_or_create_by_name(song)
+    self.find_by_name(song) || self.create(song)
+  end
+
+  def self.find_or_create_by_name(song)
+    self.find_by_name(song) || self.create(song)
   end
 
 end
