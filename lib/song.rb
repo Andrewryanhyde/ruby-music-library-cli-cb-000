1bf3082665ad01f82c1d37578dffc1d6488168eb
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-07-02 11:36:54 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,45 @@
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+  @@all = []
+
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+    self.artist = artist unless artist.nil?
+    self.genre = genre unless genre.nil?
+  end
+
+  def self.create(name)
+   new_song = Song.new(name)
+   new_song.save
+   new_song
+ end
+
+  def save
+   @@all << self
+ end
+
+ def artist=(artist)
+   @artist = artist
+   artist.add_song(self)
+ end
+
+ def genre=(genre)
+   @genre = genre
+   genre.add_song(self)
+ end
+
+ def self.all
+   @@all
+ end
+
+ def self.destroy_all
+   self.all.clear
+ end
+
+ def find_by_name(song)
+   @@all.find { |x| x.name == song}
+ end
+
+end
